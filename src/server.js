d9e8dbea0638922a0089f705da4f681d9cee5a89
Luiz Mendes <developer.luizmendes@gmail.com>
2022-03-12 15:47:41 -0300

    fix: add port

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import app from "./app.js"
-const port = 3000
+const port = process.env.PORT || 3000 
 
 const server = app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
